fba119adee8e31abe7ea434436dfe4a5292e1bca
david-hoicka <dhoicka@yahoo.com>
2016-03-18 23:15:32 +0800

    add Firefox backup for OneTab, modify readability of date/time backup tags

--- OneTab-datafile-backup.bat
+++ OneTab-datafile-backup.bat
@@ -6,13 +6,18 @@ FOR /F "TOKENS=2-4 DELIMS=/ " %%a IN ("%date%") DO SET mm=%%a&SET dd=%%b&SET yy=
 FOR /F "TOKENS=1-3 DELIMS=:." %%a IN ("%time%") DO SET hh=%%a&SET tt=%%b&SET ss=%%c
 rem test date: echo "newfilename_%yy%-%mm%-%dd%_%hh%h%tt%m%ss%s.log"
 
-rem CYBERFOX
-rem xcopy only if changed (see archive flag /M)
-echo F | xcopy C:\Users\%username%\AppData\Roaming\8pecxstudios\Cyberfox\Profiles\kjnvzpy8.default\jetpack\extension@one-tab.com\simple-storage\store.json  G:\OneDrive\z-backup-OneTab\CyberFox-OneTab-backups\store(%yy%-%mm%-%dd%_%hh%h%tt%m%ss%s).json /M
-
 rem CHROME
-rem xcopy only if changed (see archive flag /M)
-echo F | xcopy "C:\Users\%username%\AppData\Local\Google\Chrome\User Data\Default\Local Storage\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0.localstorage"  G:\OneDrive\z-backup-OneTab\Chrome-OneTab-backups\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0(%yy%-%mm%-%dd%_%hh%h%tt%m%ss%s).localstorage /M
-echo F | xcopy "C:\Users\%username%\AppData\Local\Google\Chrome\User Data\Default\Local Storage\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0.localstorage-journal"  G:\OneDrive\z-backup-OneTab\Chrome-OneTab-backups\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0(%yy%-%mm%-%dd%_%hh%h%tt%m%ss%s).localstorage-journal /M
+rem xcopy only if changed (sets archive flag /M)
+echo F | xcopy "C:\Users\%username%\AppData\Local\Google\Chrome\User Data\Default\Local Storage\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0.localstorage"  G:\OneDrive\z-backup-OneTab\Chrome-OneTab-backups\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0(%yy%-%mm%-%dd%-%hh%-%tt%-%ss%).localstorage /M
+echo F | xcopy "C:\Users\%username%\AppData\Local\Google\Chrome\User Data\Default\Local Storage\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0.localstorage-journal"  G:\OneDrive\z-backup-OneTab\Chrome-OneTab-backups\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0(%yy%-%mm%-%dd%-%hh%-%tt%-%ss%).localstorage-journal /M
+
+rem CYBERFOX
+rem xcopy only if changed (sets archive flag /M)
+echo F | xcopy C:\Users\%username%\AppData\Roaming\8pecxstudios\Cyberfox\Profiles\kjnvzpy8.default\jetpack\extension@one-tab.com\simple-storage\store.json  G:\OneDrive\z-backup-OneTab\CyberFox-OneTab-backups\store(%yy%-%mm%-%dd%-%hh%-%tt%-%ss%).json /M
+
+rem FIREFOX
+rem xcopy only if changed (sets archive flag with /M)
+echo F | xcopy C:\Users\David101\AppData\Roaming\Mozilla\Firefox\Profiles\t71tluuq.default\jetpack\extension@one-tab.com\simple-storage\store.json  G:\OneDrive\z-backup-OneTab\Firefox-OneTab-backups\store(%yy%-%mm%-%dd%-%hh%-%tt%-%ss%).json /M
+
 
 pause
