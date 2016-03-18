982aedd72da09e1813bc64cebab8edbed31fd09a
david-hoicka <dhoicka@yahoo.com>
2016-03-18 20:30:42 +0800

    add Chrome backup for OneTab, modify readability of date/time backup tags

--- OneTab-datafile-backup.bat
+++ OneTab-datafile-backup.bat
@@ -8,6 +8,11 @@ rem test date: echo "newfilename_%yy%-%mm%-%dd%_%hh%h%tt%m%ss%s.log"
 
 rem CYBERFOX
 rem xcopy only if changed (see archive flag /M)
-echo F | xcopy C:\Users\%username%\AppData\Roaming\8pecxstudios\Cyberfox\Profiles\kjnvzpy8.default\jetpack\extension@one-tab.com\simple-storage\store.json  G:\OneDrive\z-robocopy-backup-OneTab\CyberFox-OneTab-backups\store_%yy%-%mm%-%dd%_%hh%h%tt%m%ss%s.json /M
+echo F | xcopy C:\Users\%username%\AppData\Roaming\8pecxstudios\Cyberfox\Profiles\kjnvzpy8.default\jetpack\extension@one-tab.com\simple-storage\store.json  G:\OneDrive\z-backup-OneTab\CyberFox-OneTab-backups\store(%yy%-%mm%-%dd%_%hh%h%tt%m%ss%s).json /M
+
+rem CHROME
+rem xcopy only if changed (see archive flag /M)
+echo F | xcopy "C:\Users\%username%\AppData\Local\Google\Chrome\User Data\Default\Local Storage\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0.localstorage"  G:\OneDrive\z-backup-OneTab\Chrome-OneTab-backups\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0(%yy%-%mm%-%dd%_%hh%h%tt%m%ss%s).localstorage /M
+echo F | xcopy "C:\Users\%username%\AppData\Local\Google\Chrome\User Data\Default\Local Storage\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0.localstorage-journal"  G:\OneDrive\z-backup-OneTab\Chrome-OneTab-backups\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0(%yy%-%mm%-%dd%_%hh%h%tt%m%ss%s).localstorage-journal /M
 
 pause
