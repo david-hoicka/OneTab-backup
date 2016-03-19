478f63efc595c2523aa5f85383c1ce6c1d2e1e16
david-hoicka <dhoicka@yahoo.com>
2016-03-19 22:34:06 +0800

    add Vivaldi (Chrome) OneTab datafile backup

--- OneTab-datafile-backup.bat
+++ OneTab-datafile-backup.bat
@@ -11,13 +11,18 @@ rem xcopy only if changed (sets archive flag /M)
 echo F | xcopy "C:\Users\%username%\AppData\Local\Google\Chrome\User Data\Default\Local Storage\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0.localstorage"  G:\OneDrive\z-backup-OneTab\Chrome-OneTab-backups\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0(%yy%-%mm%-%dd%-%hh%-%tt%-%ss%).localstorage /M
 echo F | xcopy "C:\Users\%username%\AppData\Local\Google\Chrome\User Data\Default\Local Storage\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0.localstorage-journal"  G:\OneDrive\z-backup-OneTab\Chrome-OneTab-backups\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0(%yy%-%mm%-%dd%-%hh%-%tt%-%ss%).localstorage-journal /M
 
-rem CYBERFOX
+rem CHROME VIVALDI
 rem xcopy only if changed (sets archive flag /M)
-echo F | xcopy C:\Users\%username%\AppData\Roaming\8pecxstudios\Cyberfox\Profiles\kjnvzpy8.default\jetpack\extension@one-tab.com\simple-storage\store.json  G:\OneDrive\z-backup-OneTab\CyberFox-OneTab-backups\store(%yy%-%mm%-%dd%-%hh%-%tt%-%ss%).json /M
+echo F | xcopy "C:\Users\%username%\AppData\Local\Vivaldi\User Data\Default\Local Storage\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0.localstorage"  G:\OneDrive\z-backup-OneTab\Vivaldi-OneTab-backups\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0(%yy%-%mm%-%dd%-%hh%-%tt%-%ss%).localstorage /M
+echo F | xcopy "C:\Users\%username%\AppData\Local\Vivaldi\User Data\Default\Local Storage\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0.localstorage-journal"  G:\OneDrive\z-backup-OneTab\Vivaldi-OneTab-backups\chrome-extension_chphlpgkkbolifaimnlloiipkdnihall_0(%yy%-%mm%-%dd%-%hh%-%tt%-%ss%).localstorage-journal /M
 
 rem FIREFOX
 rem xcopy only if changed (sets archive flag with /M)
-echo F | xcopy C:\Users\David101\AppData\Roaming\Mozilla\Firefox\Profiles\t71tluuq.default\jetpack\extension@one-tab.com\simple-storage\store.json  G:\OneDrive\z-backup-OneTab\Firefox-OneTab-backups\store(%yy%-%mm%-%dd%-%hh%-%tt%-%ss%).json /M
+echo F | xcopy C:\Users\%username%\AppData\Roaming\Mozilla\Firefox\Profiles\t71tluuq.default\jetpack\extension@one-tab.com\simple-storage\store.json  G:\OneDrive\z-backup-OneTab\Firefox-OneTab-backups\store(%yy%-%mm%-%dd%-%hh%-%tt%-%ss%).json /M
+
+rem CYBERFOX
+rem xcopy only if changed (sets archive flag /M)
+echo F | xcopy C:\Users\%username%\AppData\Roaming\8pecxstudios\Cyberfox\Profiles\kjnvzpy8.default\jetpack\extension@one-tab.com\simple-storage\store.json  G:\OneDrive\z-backup-OneTab\CyberFox-OneTab-backups\store(%yy%-%mm%-%dd%-%hh%-%tt%-%ss%).json /M
 
 
 pause
